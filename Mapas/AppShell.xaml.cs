6c62d1cf6a624102b7fb8e1aebd88f8b5ca32799
NathanaelMarBa <kevin_1321124060@uptecamac.edu.mx>
2023-02-28 13:42:22 -0600

    Agregar archivos de proyecto.

---
+++ Mapas/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Mapas;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Mapas/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Map = Microsoft.Maui.Controls.Maps.Map;
+namespace Mapas;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
+public class MapTypesPageCode : ContentPage
+{
+    public MapTypesPageCode()
+    {
+        Map map = new Map();
+        Content = map;
+    }
+
+    Map mapa = new Map
+    {
+        IsShowingUser = true
+    };
+}
+
