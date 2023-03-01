f932020abb90b12713c5e8790a60d33354299386
Daniel050403 <juan_1321124035@uptecamac.edu.mx>
2023-03-01 13:23:55 -0600

    1:23

--- Mapas/MainPage.xaml.cs
+++ Mapas/MainPage.xaml.cs
@@ -15,40 +15,26 @@ public partial class MainPage : ContentPage
     {
         base.OnNavigatedTo(args);
 
-        var hanaLoc = new Location(19.8286306, -98.8960667);
-        var eder = new Location(19.6344033, -98.982768);
-        var bryan = new Location(19.5692168, -99.0266124);
-        var freddy = new Location(19.6478955, -98.9882992);
+        var eder = new Location(19.7129283, -98.9719797);
+      
+        var freddy = new Location(19.7134838, -98.9717866);
 
-        MapSpan mapSpan = MapSpan.FromCenterAndRadius(eder, Distance.FromKilometers(5));
+        MapSpan mapSpan = MapSpan.FromCenterAndRadius(eder, Distance.FromKilometers(-1));
         map.MoveToRegion(mapSpan);
         map.Pins.Add(new Pin
         {
             Label = "Casa del eder",
             Location = eder,
         });
-        MapSpan mapSpant = MapSpan.FromCenterAndRadius(freddy, Distance.FromKilometers(5));
+        MapSpan mapSpant = MapSpan.FromCenterAndRadius(freddy, Distance.FromKilometers(-1));
         map.MoveToRegion(mapSpan);
         map.Pins.Add(new Pin
         {
             Label = "Casa del fredy",
             Location = freddy,
         });
-        MapSpan mapSpand = MapSpan.FromCenterAndRadius(bryan, Distance.FromKilometers(-1));
-        map.MoveToRegion(mapSpan);
-        map.Pins.Add(new Pin
-        {
-            Label = "Casa del bryan",
-            Location = bryan,
-        });
-
-        MapSpan mapSpann = MapSpan.FromCenterAndRadius(hanaLoc, Distance.FromKilometers(-1));
-        map.MoveToRegion(mapSpan);
-        map.Pins.Add(new Pin
-        {
-            Label = "Casa del roi",
-            Location = hanaLoc,
-        });
+ 
+       
 
 
     }
@@ -59,8 +45,10 @@ public class MapTypesPageCode : ContentPage
 {
     public MapTypesPageCode()
     {
-        Map map = new();
-        Content = map;
+        Map map = new Map
+        {
+            MapType = MapType.Street
+        };
     }
 
 }
