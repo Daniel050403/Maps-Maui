1b1d87e090989156d38e78950d3c8a22607c91d0
Daniel050403 <juan_1321124035@uptecamac.edu.mx>
2023-03-01 12:41:50 -0600

    final

--- Mapas/MainPage.xaml.cs
+++ Mapas/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Map = Microsoft.Maui.Controls.Maps.Map;
+﻿using Microsoft.Maui.Controls.Maps;
+using Microsoft.Maui.Maps;
+using Map = Microsoft.Maui.Controls.Maps.Map;
 namespace Mapas;
 
 public partial class MainPage : ContentPage
@@ -9,19 +11,57 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+
+        var hanaLoc = new Location(19.8286306, -98.8960667);
+        var eder = new Location(19.6344033, -98.982768);
+        var bryan = new Location(19.5692168, -99.0266124);
+        var freddy = new Location(19.6478955, -98.9882992);
+
+        MapSpan mapSpan = MapSpan.FromCenterAndRadius(eder, Distance.FromKilometers(5));
+        map.MoveToRegion(mapSpan);
+        map.Pins.Add(new Pin
+        {
+            Label = "Casa del eder",
+            Location = eder,
+        });
+        MapSpan mapSpant = MapSpan.FromCenterAndRadius(freddy, Distance.FromKilometers(5));
+        map.MoveToRegion(mapSpan);
+        map.Pins.Add(new Pin
+        {
+            Label = "Casa del fredy",
+            Location = freddy,
+        });
+        MapSpan mapSpand = MapSpan.FromCenterAndRadius(bryan, Distance.FromKilometers(-1));
+        map.MoveToRegion(mapSpan);
+        map.Pins.Add(new Pin
+        {
+            Label = "Casa del bryan",
+            Location = bryan,
+        });
+
+        MapSpan mapSpann = MapSpan.FromCenterAndRadius(hanaLoc, Distance.FromKilometers(-1));
+        map.MoveToRegion(mapSpan);
+        map.Pins.Add(new Pin
+        {
+            Label = "Casa del roi",
+            Location = hanaLoc,
+        });
+
+
+    }
+
 }
 
 public class MapTypesPageCode : ContentPage
 {
     public MapTypesPageCode()
     {
-        Map map = new Map();
+        Map map = new();
         Content = map;
     }
 
-    Map mapa = new Map
-    {
-        IsShowingUser = true
-    };
 }
 
